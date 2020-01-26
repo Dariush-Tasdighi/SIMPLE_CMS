5eb54c2ec620112ae93642453820f53c2982a198
mona.ghadimi <mona.ghadimi.1369@gmail.com>
2020-01-26 22:29:22 +0330

    Making Some Changes!

---
+++ Models/Post.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+
+namespace Models
+{
+	public class Post: Base.Entity
+	{
+
+		public Post():base()
+		{
+		}
+
+
+		// **********
+		/// <summary>
+		/// توضیحات
+		/// </summary>
+		[System.ComponentModel.DataAnnotations.Display
+			(ResourceType = typeof(Resources.DataDictionary),
+			Name = nameof(Resources.DataDictionary.Description))]
+
+		public string Description { get; set; }
+		// **********
+
+
+		// **********
+		/// <summary>
+		/// رمز عبور
+		/// </summary>
+		[System.ComponentModel.DataAnnotations.Display
+			(ResourceType = typeof(Resources.DataDictionary),
+			Name = nameof(Resources.DataDictionary.Password))]
+
+		public string Password { get; set; }
+		// **********
+
+	}
+}
