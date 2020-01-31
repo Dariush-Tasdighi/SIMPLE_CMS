7c9e4cf464506a4335d741381793c7ddbcc1021a
alireza golshan <alireza.golshan0@gmail.com>
2020-01-31 10:00:28 -0800

    BaseController was completed.

--- MY_APPLICATION/Infrastructure/BaseController.cs
+++ MY_APPLICATION/Infrastructure/BaseController.cs
@@ -1,9 +1,41 @@
 ﻿namespace Infrastructure
 {
-	public class BaseController : System.Web.Mvc.Controller
-	{
-		public BaseController() : base()
-		{
-		}
-	}
+    public class BaseController : System.Web.Mvc.Controller
+    {
+        public BaseController() : base()
+        {
+        }
+
+        private Models.DatabaseContext myDatabaseContext;
+
+        public Models.DatabaseContext MydataBaseContext
+        {
+            get
+            {
+                if (myDatabaseContext == null)
+                {
+                    myDatabaseContext = new Models.DatabaseContext();
+                }
+                return myDatabaseContext;
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (myDatabaseContext != null)
+                {
+                    myDatabaseContext.Dispose();
+
+                    myDatabaseContext = null;
+                }
+            }
+
+            base.Dispose(disposing);
+        }
+
+
+
+    }
 }
